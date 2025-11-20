bcd13c6a811cd60978f1b1676156f85846e4266d
pradeechowdary <142884490+pradeechowdary@users.noreply.github.com>
2025-11-20 14:04:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   console.log("JS Loaded!");
 
-  const API_URL = "https://script.google.com/macros/s/AKfycbzQvBSxa1qy3BjmRgeGu2qRw2V2RPw8LBGe47erhmftcXB42tuPi1tBce-Zn2sUDLEAKg/exec";
+  const API_URL = "https://script.google.com/macros/s/AKfycbxR4jPLqJSvOAPEnMvNqCISvHX8Vien8tKlneQFLN3sUYqGjBAPKdb4k2EC1SLyjF_B-w/exec";
 
   const userId = "user-" + Math.random().toString(36).substring(2, 10);
   const sessionId = "sess-" + Math.random().toString(36).substring(2, 10);
